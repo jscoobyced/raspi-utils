4dd2062df95189bd200496558f13cad14beed99d
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-11-06 20:05:01 +0700

    Update script to use color indicator for temperature

--- cooling-hat-oled/cooling.py
+++ cooling-hat-oled/cooling.py
@@ -1,3 +1,8 @@
+import subprocess
+from PIL import ImageFont
+from PIL import ImageDraw
+from PIL import Image
+import Adafruit_SSD1306
 import Adafruit_GPIO.I2C as I2C
 
 import time
@@ -5,18 +10,10 @@ import os
 import smbus
 bus = smbus.SMBus(1)
 
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-import subprocess
-
 hat_addr = 0x0d
 rgb_effect_reg = 0x04
 fan_reg = 0x08
-fan_state = 2
+fan_state = 3
 count = 0
 
 # Raspberry Pi pin configuration:
@@ -42,7 +39,7 @@ image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 
 # Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
 # Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
@@ -59,17 +56,27 @@ font = ImageFont.load_default()
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
 
+
 def setFanSpeed(speed):
-    bus.write_byte_data(hat_addr, fan_reg, speed&0xff)
+    bus.write_byte_data(hat_addr, fan_reg, speed & 0xff)
+
+
+def setRGB(r, g, b):
+    bus.write_byte_data(hat_addr, 0x00, 0xff)
+    bus.write_byte_data(hat_addr, 0x01, r & 0xff)
+    bus.write_byte_data(hat_addr, 0x02, g & 0xff)
+    bus.write_byte_data(hat_addr, 0x03, b & 0xff)
+
 
 def getCPULoadRate():
     f1 = os.popen("cat /proc/stat", 'r')
     stat1 = f1.readline()
     count = 10
     data_1 = []
-    for i  in range (count):
+    for i in range(count):
         data_1.append(int(stat1.split(' ')[i+2]))
-    total_1 = data_1[0]+data_1[1]+data_1[2]+data_1[3]+data_1[4]+data_1[5]+data_1[6]+data_1[7]+data_1[8]+data_1[9]
+    total_1 = data_1[0]+data_1[1]+data_1[2]+data_1[3] + \
+        data_1[4]+data_1[5]+data_1[6]+data_1[7]+data_1[8]+data_1[9]
     idle_1 = data_1[3]
 
     time.sleep(1)
@@ -77,41 +84,40 @@ def getCPULoadRate():
     f2 = os.popen("cat /proc/stat", 'r')
     stat2 = f2.readline()
     data_2 = []
-    for i  in range (count):
+    for i in range(count):
         data_2.append(int(stat2.split(' ')[i+2]))
-    total_2 = data_2[0]+data_2[1]+data_2[2]+data_2[3]+data_2[4]+data_2[5]+data_2[6]+data_2[7]+data_2[8]+data_2[9]
+    total_2 = data_2[0]+data_2[1]+data_2[2]+data_2[3] + \
+        data_2[4]+data_2[5]+data_2[6]+data_2[7]+data_2[8]+data_2[9]
     idle_2 = data_2[3]
 
     total = int(total_2-total_1)
     idle = int(idle_2-idle_1)
     usage = int(total-idle)
-    #print("idle:"+str(idle)+"  total:"+str(total))
-    usageRate =int(float(usage * 100/ total))
-    #print("usageRate:%d"%usageRate)
+    usageRate = int(float(usage * 100 / total))
     return "CPU:"+str(usageRate)+"%"
 
 
 def setOLEDshow():
     # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     #cmd = "top -bn1 | grep load | awk '{printf \"CPU:%.0f%%\", $(NF-2)*100}'"
     #CPU = subprocess.check_output(cmd, shell = True)
     CPU = getCPULoadRate()
 
     cmd = os.popen('./vcgencmd measure_temp').readline()
-    CPU_TEMP = cmd.replace("temp=","Temp:").replace("'C\n","C")
+    CPU_TEMP = cmd.replace("temp=", "Temp:").replace("'C\n", "C")
     global g_temp
-    g_temp = float(cmd.replace("temp=","").replace("'C\n",""))
+    g_temp = float(cmd.replace("temp=", "").replace("'C\n", ""))
 
     cmd = "free -m | awk 'NR==2{printf \"RAM:%s/%s MB \", $2-$3,$2}'"
-    MemUsage = subprocess.check_output(cmd, shell = True)
+    MemUsage = subprocess.check_output(cmd, shell=True)
 
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk:%d/%dMB\", ($2-$3)*1024,$2*1024}'"
-    Disk = subprocess.check_output(cmd, shell = True)
+    Disk = subprocess.check_output(cmd, shell=True)
 
     cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True)
+    IP = subprocess.check_output(cmd, shell=True)
 
     # Write two lines of text.
 
@@ -126,18 +132,26 @@ def setOLEDshow():
     disp.display()
     time.sleep(.1)
 
+
 setFanSpeed(0x00)
 
 while True:
-    setOLEDshow()	
+    setOLEDshow()
     if g_temp >= 55:
-        if fan_state != 1:
+        if fan_state != 2:
             setFanSpeed(0x01)
-            fan_state = 1        
+            fan_state = 2
+            setRGB(0xff, 0x00, 0x00)
+    elif g_temp <= 54:
+        if fan_state != 1:
+            setFanSpeed(0x00)
+            fan_state = 1
+            setRGB(0x00, 0xff, 0x00)
     elif g_temp <= 48:
         if fan_state != 0:
             setFanSpeed(0x00)
             fan_state = 0
-    
+            setRGB(0x00, 0x00, 0xff)
+
     count += 1
-    time.sleep(.5)
+    time.sleep(2)
